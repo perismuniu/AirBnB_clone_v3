229cd5f60365c2f96db423ad19a185929c4508c8
perismuniu <peris.muniu@gmail.com>
2024-01-07 04:16:54 -0800

    3. Status of your API

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-from flask import Flask
+# api/v1/app.py
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 
@@ -9,11 +10,11 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 @app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Teardown method to close storage"""
+def teardown(exception):
+    """Closes the storage on teardown."""
     storage.close()
 
 if __name__ == "__main__":
-    host = os.environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.environ.get('HBNB_API_PORT', 5000))
+    host = "0.0.0.0" if not os.getenv("HBNB_API_HOST") else os.getenv("HBNB_API_HOST")
+    port = 5000 if not os.getenv("HBNB_API_PORT") else int(os.getenv("HBNB_API_PORT"))
     app.run(host=host, port=port, threaded=True)
